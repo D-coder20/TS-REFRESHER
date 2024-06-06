e86508632a1bdf728e512e1c1d898b422e93f50c
D-coder20 <debpriya.20@gmail.com>
2024-06-06 22:40:28 +0530

    Added warning message to the application

--- react-ts-basics/src/components/CourseGoalList.tsx
+++ react-ts-basics/src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
+import { type ReactNode } from "react";
 import CourseGoals from "./CourseGoals";
 import { type CourseGoal as CGoal } from "../App";
+import InfoBox from "./InfoBox";
 
 type CourseGoalListProps = {
   goals: CGoal[];
@@ -10,21 +12,40 @@ export default function CourseGoalList({
   goals,
   onDeleteGoal,
 }: CourseGoalListProps) {
+  if (goals.length == 0) {
+    return (
+      <InfoBox mode="hint">
+        You have no course goals yet. Start adding some!
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning" severity="high">
+        You're collecting lot of goals. Don't put too much on your plate
+      </InfoBox>
+    );
+  }
   return (
-    <article>
-      <ul>
-        {goals.map((goal) => (
-          <li key={goal.id}>
-            <CourseGoals
-              id={goal.id}
-              title={goal.title}
-              onDelete={onDeleteGoal}
-            >
-              <p>{goal.description}</p>
-            </CourseGoals>
-          </li>
-        ))}
-      </ul>
-    </article>
+    <>
+      {warningBox}
+      <article>
+        <ul>
+          {goals.map((goal) => (
+            <li key={goal.id}>
+              <CourseGoals
+                id={goal.id}
+                title={goal.title}
+                onDelete={onDeleteGoal}
+              >
+                <p>{goal.description}</p>
+              </CourseGoals>
+            </li>
+          ))}
+        </ul>
+      </article>
+    </>
   );
 }
